ab46c0974b5d636dc695709c21bbc37319811890
Darshan Satya <darshans1989@gmail.com>
2021-05-16 11:40:27 -0400

    refactored using routematch

--- src/components/pages/QuoteDetail.js
+++ src/components/pages/QuoteDetail.js
@@ -1,4 +1,5 @@
-import { Route, useParams } from "react-router";
+import { Route, useParams, useRouteMatch } from "react-router";
+import { Link } from "react-router-dom";
 import Comments from "../comments/Comments";
 import HighlightedQuote from "../quotes/HighlightedQuote";
 import NoQuotesFound from "../quotes/NoQuotesFound";
@@ -8,6 +9,9 @@ const QuoteDetail = (props) => {
   const params = useParams();
   const quoteId = params.id;
   const quote = DUMMY_QUOTES.find((quote) => quote.id === quoteId);
+  const match = useRouteMatch();
+
+  console.log(match);
 
   if (!quote) {
     return <NoQuotesFound />;
@@ -15,7 +19,17 @@ const QuoteDetail = (props) => {
   return (
     <>
       <HighlightedQuote text={quote.text} author={quote.author} />
-      <Route path={`/quotes/${quoteId}/comments`}>
+
+      <Route path={`${match.path}`} exact>
+        <div className="centered">
+          <Link className="btn--flat" to={`${match.url}/comments`}>
+            {" "}
+            Load Comments{" "}
+          </Link>
+        </div>
+      </Route>
+
+      <Route path={`${match.path}/comments`}>
         <Comments />
       </Route>
     </>
--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -17,22 +17,30 @@ const sortQuotes = (quotes, ascending) => {
 const QuoteList = (props) => {
   const history = useHistory();
   const location = useLocation();
-  console.log(location)
+  console.log(location);
 
   const queryParams = new URLSearchParams(location.search);
 
-  const isSortingAscending = queryParams.get('sort') === 'asc';
+  const isSortingAscending = queryParams.get("sort") === "asc";
 
   const sortedQuotes = sortQuotes(props.quotes, isSortingAscending);
 
   const changeSortingHandler = () => {
-    history.push("/quotes?sort=" + (isSortingAscending ? 'desc' : 'asc'));
+    history.push({
+      pathname: location.pathname,
+      search: `?sort=${isSortingAscending ? "desc" : "asc"}`
+    });
+    // history.push(
+    //   `${location.pathname}?sort=${isSortingAscending ? "desc" : "asc"}`
+    // );
   };
 
   return (
     <Fragment>
       <div className={classes.sorting}>
-        <button onClick={changeSortingHandler}>Sort {isSortingAscending ? 'Desc' : 'Ascending'}</button>
+        <button onClick={changeSortingHandler}>
+          Sort {isSortingAscending ? "Desc" : "Ascending"}
+        </button>
       </div>
       <ul className={classes.list}>
         {sortedQuotes &&
