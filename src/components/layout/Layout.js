288d7720f5bf689146b248a4e7e2d7f253400803
Darshan Satya <darshans1989@gmail.com>
2021-05-16 13:17:03 -0400

    Lazy loading components

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,33 +1,44 @@
+import React, { Suspense } from "react";
 import { Redirect, Route, Switch } from "react-router";
-import AddQuote from "../pages/AddQuote";
-import AllQuotes from "../pages/AllQuotes";
-import NotFound from "../pages/NotFound";
-import QuoteDetail from "../pages/QuoteDetail";
+import LoadingSpinner from "../UI/LoadingSpinner";
 import styles from "./Layout.module.css";
 import MainNavigation from "./MainNavigation";
+
+const AddQuote = React.lazy(() => import("../pages/AddQuote"));
+const QuoteDetail = React.lazy(() => import("../pages/QuoteDetail"));
+const NotFound = React.lazy(() => import("../pages/NotFound"));
+const AllQuotes = React.lazy(() => import("../pages/AllQuotes"));
 
 const Layout = () => {
   return (
     <>
       <MainNavigation />
       <main className={styles.main}>
-        <Switch>
-          <Route path="/" exact>
-            <Redirect to="/quotes" />
-          </Route>
-          <Route path="/quotes" exact>
-            <AllQuotes />
-          </Route>
-          <Route path="/addQuote">
-            <AddQuote />
-          </Route>
-          <Route path="/quotes/:id">
-            <QuoteDetail />
-          </Route>
-          <Route path='*'>
-              <NotFound/>
-          </Route>
-        </Switch>
+        <Suspense
+          fallback={
+            <div className="centered">
+              <LoadingSpinner />
+            </div>
+          }
+        >
+          <Switch>
+            <Route path="/" exact>
+              <Redirect to="/quotes" />
+            </Route>
+            <Route path="/quotes" exact>
+              <AllQuotes />
+            </Route>
+            <Route path="/addQuote">
+              <AddQuote />
+            </Route>
+            <Route path="/quotes/:id">
+              <QuoteDetail />
+            </Route>
+            <Route path="*">
+              <NotFound />
+            </Route>
+          </Switch>
+        </Suspense>
       </main>
     </>
   );
